69d322800f2fdf6c56ef8c94a79888db1782e322
Lee Connelly <lee@leeconnelly.dev>
2023-04-01 17:01:07 +0100

    Added like button

--- resources/js/Pages/Songs/Index.vue
+++ resources/js/Pages/Songs/Index.vue
@@ -1,3 +1,34 @@
+<script setup>
+import { usePage } from '@inertiajs/vue3'
+import { computed } from '@vue/reactivity'
+import axios from 'axios'
+
+defineProps({
+  songs: Array,
+})
+
+const user = computed(() => usePage().props.auth.user)
+
+async function like(song) {
+  await axios.put(route('liked-songs.update', song))
+  song.liked = true
+}
+
+async function unlike(song) {
+  await axios.delete(route('liked-songs.destroy', song))
+  song.liked = false
+}
+</script>
+
 <template>
   <div>songs page</div>
+  <ul>
+    <li v-for="song in songs" :key="song.id" class="flex gap-4">
+      <p>{{ song.name }}</p>
+      <template v-if="user">
+        <button v-if="song.liked === false" @click="like(song)" class="px-2 bg-green-200">like</button>
+        <button v-else @click="unlike(song)" class="px-2 bg-green-200">unlike</button>
+      </template>
+    </li>
+  </ul>
 </template>
---
+++ resources/js/Pages/Songs/Liked/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  songs: Array,
+})
+</script>
+
+<template>
+  <div>liked songs page</div>
+  <ul>
+    <li v-for="song in songs" :key="song.id">
+      <p>{{ song.name }}</p>
+    </li>
+  </ul>
+</template>
