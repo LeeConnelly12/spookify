2449e61aa22a03f5d49adda1b27eb134f8bda45b
Lee Connelly <lee@leeconnelly.dev>
2023-04-04 00:35:27 +0100

    Style sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,7 +1,35 @@
-<script setup></script>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
 
 <template>
-  <aside class="w-64 bg-black">
-    <nav></nav>
+  <aside class="w-64 bg-black py-6 pl-6">
+    <nav class="grid text-sm font-bold">
+      <Link href="/" class="text-2xl font-bold">Spookify</Link>
+      <div class="mt-6">
+        <Link href="/" :class="route().current('home') ? 'opacity-100' : 'opacity-70'" class="flex h-10 items-center gap-4 transition-opacity duration-300 hover:opacity-100">
+          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+          Home
+        </Link>
+        <Link href="/search" class="flex h-10 items-center gap-4 opacity-70 transition-opacity duration-300 hover:opacity-100">
+          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+          Search
+        </Link>
+        <Link href="/playlists" class="flex h-10 items-center gap-4 opacity-70 transition-opacity duration-300 hover:opacity-100">
+          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 w-7">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+          </svg>
+          Your Library
+        </Link>
+      </div>
+      <div class="mt-6">
+        <Link href="/playlists/create" class="flex h-10 items-center opacity-70 transition-opacity duration-300 hover:opacity-100">Create Playlist</Link>
+        <Link href="/songs" :class="route().current('songs') ? 'opacity-100' : 'opacity-70'" class="flex h-10 items-center transition-opacity duration-300 hover:opacity-100">Liked Songs</Link>
+      </div>
+    </nav>
   </aside>
 </template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,7 +6,7 @@ import Player from '@/Components/Player.vue'
 <template>
   <div class="grid h-screen grid-cols-[auto,1fr] grid-rows-[1fr,auto]">
     <Sidebar />
-    <main class="bg-gray-900 px-8 py-4">
+    <main class="bg-gradient-to-b from-[#222] to-[#121212] px-8 py-4">
       <slot />
     </main>
     <Player class="col-span-full" />
