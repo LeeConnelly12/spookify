bc714e86de083d7a36645d105125ffd60fcf9f0a
Lee Connelly <lee@leeconnelly.dev>
2023-04-07 01:19:19 +0100

    Redirect to homepage

--- routes/web.php
+++ routes/web.php
@@ -97,10 +97,6 @@ Route::delete('/songs/{song}/unlike', [LikedSongController::class, 'destroy'])
     ->name('liked-songs.destroy')
     ->middleware('auth');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
