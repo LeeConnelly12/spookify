3ec62a1afdb1ce50ca919eaf5bb062b471945449
Lee Connelly <lee@leeconnelly.dev>
2023-04-26 01:31:45 +0100

    Fix player to bottom of screen

--- resources/js/Components/Player.vue
+++ resources/js/Components/Player.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-  <footer class="h-24 border-t border-t-[#282828] bg-[#181818]"></footer>
+  <footer class="fixed bottom-0 left-0 right-0 h-24 border-t border-t-[#282828] bg-[#181818]"></footer>
 </template>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -17,50 +17,50 @@ const closeModal = [() => (menu.value = false), { ignore: [ignoreButton] }]
 </script>
 
 <template>
-  <div class="grid h-screen grid-cols-[auto,1fr] grid-rows-[1fr,auto]">
+  <div class="grid min-h-screen grid-cols-[auto,1fr] grid-rows-[1fr,auto] pb-24">
     <Sidebar />
     <main class="bg-gradient-to-b from-[#222] to-[#121212] px-8 py-4">
       <div class="grid grid-cols-[max-content,max-content,1fr,max-content] items-center gap-4">
         <!-- Navigation arrows -->
-        <button class="grid w-8 h-8 bg-black rounded-full place-items-center" type="button">
-          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <button class="grid h-8 w-8 place-items-center rounded-full bg-black" type="button">
+          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
           </svg>
         </button>
-        <button class="grid w-8 h-8 bg-black rounded-full place-items-center" type="button">
-          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <button class="grid h-8 w-8 place-items-center rounded-full bg-black" type="button">
+          <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
           </svg>
         </button>
 
         <!-- Profile dropdown -->
-        <button v-if="user" @click="menu = !menu" ref="ignoreButton" type="button" class="relative flex items-center h-8 col-start-4 gap-2 pr-2 leading-none bg-black rounded-full">
+        <button v-if="user" @click="menu = !menu" ref="ignoreButton" type="button" class="relative col-start-4 flex h-8 items-center gap-2 rounded-full bg-black pr-2 leading-none">
           <div class="rounded-full border-2 border-black bg-[#535353] p-[2px]">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
           </div>
           <p class="font-bold">{{ user.name }}</p>
-          <svg class="w-4 h-4 transform" :class="{ 'rotate-180': menu }" viewBox="0 0 16 16">
+          <svg class="h-4 w-4 transform" :class="{ 'rotate-180': menu }" viewBox="0 0 16 16">
             <path d="m14 6-6 6-6-6h12z" fill="white" />
           </svg>
-          <ul v-if="menu" v-on-click-outside="closeModal" class="absolute right-0 grid justify-start p-1 mt-2 text-sm text-left bg-gray-500 rounded-md shadow-md top-full">
+          <ul v-if="menu" v-on-click-outside="closeModal" class="absolute right-0 top-full mt-2 grid justify-start rounded-md bg-gray-500 p-1 text-left text-sm shadow-md">
             <li>
-              <Link href="/account" class="flex items-center justify-between w-48 h-10 px-3 hover:bg-gray-400">Account</Link>
+              <Link href="/account" class="flex h-10 w-48 items-center justify-between px-3 hover:bg-gray-400">Account</Link>
             </li>
             <li>
-              <Link href="/profile" class="flex items-center w-48 h-10 px-3 hover:bg-gray-400">Profile</Link>
+              <Link href="/profile" class="flex h-10 w-48 items-center px-3 hover:bg-gray-400">Profile</Link>
             </li>
             <li>
-              <Link href="/settings" class="flex items-center w-48 h-10 px-3 border-b border-b-white border-opacity-10 hover:bg-gray-400">Settings</Link>
+              <Link href="/settings" class="flex h-10 w-48 items-center border-b border-b-white border-opacity-10 px-3 hover:bg-gray-400">Settings</Link>
             </li>
             <li>
-              <Link href="/logout" method="post" as="button" class="flex items-center w-48 h-10 px-3 hover:bg-gray-400">Log out</Link>
+              <Link href="/logout" method="post" as="button" class="flex h-10 w-48 items-center px-3 hover:bg-gray-400">Log out</Link>
             </li>
           </ul>
         </button>
 
-        <div class="flex col-start-4 gap-8" v-else>
+        <div class="col-start-4 flex gap-8" v-else>
           <Link href="/register">Sign up</Link>
           <Link href="/login">Log in</Link>
         </div>
