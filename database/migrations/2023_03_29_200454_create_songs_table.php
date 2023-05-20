4df3d111c317764a32bb468dc6adaf4d247cf527
Lee Connelly <lee@leeconnelly.dev>
2023-05-20 17:58:58 +0100

    Update duration to be a string

--- database/migrations/2023_03_29_200454_create_songs_table.php
+++ database/migrations/2023_03_29_200454_create_songs_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->string('name', 25);
             $table->unsignedSmallInteger('year');
-            $table->unsignedSmallInteger('duration');
+            $table->string('duration', 5);
             $table->timestamps();
         });
     }
